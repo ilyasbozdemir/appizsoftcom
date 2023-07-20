f0ac17d3f4ea3a447a69ff844d4dbe07faae7dde
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-07-20 19:18:47 +0300

    updated

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,10 +3,49 @@ import theme from "../src/theme";
 import { ColorModeScript } from "@chakra-ui/react";
 import GoogleTagManagerBody from "../plugins/GoogleTagManagerBody";
 import ExternalFonts from "../fonts/ExternalFonts";
-
 import React from "react";
 import Analytics from "../configuration/Analytics";
 import { site } from "../constants/site";
+
+export default class MyDocument extends Document {
+  render() {
+    const { langValue } = this.props;
+
+    return (
+      <Html lang={langValue || site.lang}>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <meta name="language" content="Turkish" />
+          <meta
+            name="viewport"
+            content="width=device-width,minimum-scale=1,initial-scale=1"
+          />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="revisit-after" content="3 days" />
+          <link rel="icon" href="/favicon.png" type="image/png" />
+          <ExternalFonts />
+          <Analytics />
+          <link rel="stylesheet" href="/styles/globals.css" />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdSchema) }}
+          />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdSchema2) }}
+          />
+        </Head>
+        <body>
+          <GoogleTagManagerBody code={site.analyticsCodes.gtmCode} />
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
 const jsonLdSchema = {
   "@context": "https://schema.org",
@@ -57,7 +96,6 @@ const jsonLdSchema = {
   ],
   inLanguage: "tr",
 };
-
 const jsonLdSchema2 = {
   "@context": "https://schema.org",
   "@type": "Organization",
@@ -83,72 +121,6 @@ const jsonLdSchema2 = {
   ],
   sameAs: site.sosyalMediaLinks.map((link) => link.link),
 };
-
-export default class MyDocument extends Document {
-  render() {
-    const { langValue } = this.props;
-
-    return (
-      <Html lang={langValue || site.lang}>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-          <meta name="language" content="Turkish" />
-
-          <meta  name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,maximum-scale=1"/>
-
-          
-
-          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="revisit-after" content="3 days" />
-          <link rel="icon" href="/favicon.png" type="image/png" />
-
-          <ExternalFonts />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            charset="UTF-8"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
-
-
-{
-  /*
-     <Analytics />
-  */
-}
-
-       
-
-
-
-          <link rel="stylesheet" href="/styles/globals.css" />
-
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdSchema) }}
-          />
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdSchema2) }}
-          />
-        </Head>
-
-        <body style={{ bg: "#111" }}>
-          <GoogleTagManagerBody code={site.analyticsCodes.gtmCode} />
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
 
 MyDocument.getInitialProps = async (ctx) => {
   const originalRenderPage = ctx.renderPage;
