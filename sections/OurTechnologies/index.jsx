53eea6dccf9305aa57c860d7b9d3cf4096d546d3
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-07-21 12:09:15 +0300

    updated

--- sections/OurTechnologies/index.jsx
+++ sections/OurTechnologies/index.jsx
@@ -11,7 +11,7 @@ import { technologies } from "../../constants/technologies";
 import Slider from "react-slick";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const lang = `tr`;
@@ -29,7 +29,7 @@ function OurTechnologies() {
     slidesToShow: 9,
     slidesToScroll: 4,
     focusOnSelect: true,
-    draggable: true, 
+    draggable: true,
     responsive: [
       {
         breakpoint: 1024,
@@ -37,7 +37,7 @@ function OurTechnologies() {
           slidesToShow: 7,
           slidesToScroll: 7,
           infinite: true,
-          draggable: true, 
+          draggable: true,
         },
       },
       {
@@ -46,7 +46,7 @@ function OurTechnologies() {
           slidesToShow: 5,
           slidesToScroll: 2,
           initialSlide: 4,
-          draggable: true, 
+          draggable: true,
         },
       },
       {
@@ -54,7 +54,7 @@ function OurTechnologies() {
         settings: {
           slidesToShow: 4,
           slidesToScroll: 3,
-          draggable: true, 
+          draggable: true,
         },
       },
       {
@@ -62,12 +62,11 @@ function OurTechnologies() {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          draggable: true, 
+          draggable: true,
         },
       },
     ],
   };
-  
 
   const [isDragging, setIsDragging] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
@@ -80,79 +79,83 @@ function OurTechnologies() {
   };
 
   return (
-    <Box mt={10}>
-      <Box mx={3} display="flex" alignItems="center">
-        <Divider flex="1" borderWidth="1px" borderColor="gray.300" />
-        <Box flex="1" textAlign="center">
-          <Heading
-            fontFamily={"Montserrat"}
-            as="h2"
-            fontSize={{ base: "24px", md: "26px", lg: "28px", xl: "32px" }}
-          >
-            Teknolojiler
-          </Heading>
+    <>
+      <Box mt={10} display={'none'}>
+        <Box mx={3} display="flex" alignItems="center">
+          <Divider flex="1" borderWidth="1px" borderColor="gray.300" />
+          <Box flex="1" textAlign="center">
+            <Heading
+              fontFamily={"Montserrat"}
+              as="h2"
+              fontSize={{ base: "24px", md: "26px", lg: "28px", xl: "32px" }}
+            >
+              Teknolojiler
+            </Heading>
+          </Box>
+          <Divider flex="1" borderWidth="1px" borderColor="gray.300" />
         </Box>
-        <Divider flex="1" borderWidth="1px" borderColor="gray.300" />
+
+        <Box
+          px={8}
+          py={20}
+          mx="auto"
+          bg="white"
+          _dark={{
+            bg: "gray.800",
+          }}
+        >
+          <Slider {...settings}>
+            {technologies.map((image, index) => (
+              <React.Fragment key={index}>
+                <div
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                  onMouseDown={handleMouseDown}
+                  onMouseUp={handleMouseUp}
+                >
+                  <Link
+                    href={`${lang}/technologies#${image.id}`}
+                    passHref
+                    legacyBehavior
+                  >
+                    <a
+                      style={{
+                        pointerEvents: isDragging ? "none" : "auto",
+                        cursor: isDragging ? "grabbing" : "pointer",
+                      }}
+                    >
+                      <ChakraImage
+                        id={image.id}
+                        src={image.imageUrl}
+                        alt={`${image.title}`}
+                        width={50}
+                        height={50}
+                        filter={"grayscale(99%)"}
+                      />
+                    </a>
+                  </Link>
+                </div>
+              </React.Fragment>
+            ))}
+          </Slider>
+          <Flex mt={10} justify={"center"}>
+            <Text
+              onClick={() => {
+                router.push(`${lang}/technologies`);
+              }}
+              color={"blue"}
+              fontWeight={"semibold"}
+              mt={3}
+              cursor={"pointer"}
+            >
+              Tam hizmet teknolojilerimizi görüntüleyin →
+            </Text>
+          </Flex>
+        </Box>
       </Box>
 
-      <Box
-        px={8}
-        py={20}
-        mx="auto"
-        bg="white"
-        _dark={{
-          bg: "gray.800",
-        }}
-      >
-        <Slider {...settings}>
-          {technologies.map((image, index) => (
-            <React.Fragment key={index}>
-              <div
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-                onMouseDown={handleMouseDown}
-                onMouseUp={handleMouseUp}
-              >
-                <Link
-                  href={`${lang}/technologies#${image.id}`}
-                  passHref
-                  legacyBehavior
-                >
-                  <a
-                    style={{
-                      pointerEvents: isDragging ? "none" : "auto",
-                      cursor: isDragging ? "grabbing" : "pointer",
-                    }}
-                  >
-                    <ChakraImage
-                      id={image.id}
-                      src={image.imageUrl}
-                      alt={`${image.title}`}
-                      width={50}
-                      height={50}
-                      filter={"grayscale(99%)"}
-                    />
-                  </a>
-                </Link>
-              </div>
-            </React.Fragment>
-          ))}
-        </Slider>
-        <Flex mt={10} justify={"center"}>
-          <Text
-            onClick={() => {
-              router.push(`${lang}/technologies`);
-            }}
-            color={"blue"}
-            fontWeight={"semibold"}
-            mt={3}
-            cursor={"pointer"}
-          >
-            Tam hizmet teknolojilerimizi görüntüleyin →
-          </Text>
-        </Flex>
-      </Box>
-    </Box>
+
+    </>
   );
 }
 
