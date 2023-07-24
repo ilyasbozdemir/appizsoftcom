6ec714d600a005d0f892f72a15736cab34f8552e
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-07-24 15:15:34 +0300

    updated  Analytics

--- configuration/Analytics.jsx
+++ configuration/Analytics.jsx
@@ -10,16 +10,16 @@ import TiktokPixel from "../plugins/TiktokPixel";
 function Analytics() {
   return (
     <>
+      {/* 
+      
       <PinterestAnalytics code={site.analyticsCodes.pinterestAnalyticsCode} />
       <GoogleAnalytics code={site.analyticsCodes.googleAnalyticsCode} />
       <FacebookPixel code={site.analyticsCodes.facebookPixelCode} />
       <GoogleTagManager code={site.analyticsCodes.gtmCode} />
 
-      {/*
+     
         <TiktokPixel code={site.analyticsCodes.ttCode} />
       */}
-    
-      
     </>
   );
 }
