b0fab0dd96843414f86ce710da412ef9ce0fecff
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-07-24 15:17:42 +0300

    updated next config

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ module.exports = {
         headers: [
           {
             key: "Cache-Control",
-            value: "public, max-age=3600", // 1 saatlik önbellek süresi
+            value: "public, max-age=31536000", // 1 saatlik önbellek süresi
           },
         ],
       },
