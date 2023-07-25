3729cc6571380fde42b61f654ecb343642a3b67a
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-07-25 16:51:38 +0300

    updated

--- constants/testimonialsData2.jsx
+++ constants/testimonialsData2.jsx
@@ -1,22 +1,37 @@
 export const testimonialsData2 = [
- 
   {
-    name: 'Emre Yılmaz',
-    position: 'Lead Developer',
-    company: 'CodeMasterTech',
+    name: "Emre Yılmaz",
+    position: "Lead Developer",
+    company: "CodeMasterTech",
     rating: 5,
-    content: 'Appzisoft  ile birlikte çalıştığımız proje, gerçekten harika bir deneyimdi. Jena Karlis ve ekibi, isteklerimizi anlamak ve ihtiyaçlarımıza uygun bir çözüm sunmak için sürekli çaba gösterdiler. Proje süresince sağlam bir iletişim ve proaktif bir yaklaşım sergilediler. Sonuç, kullanıcı dostu ve sorunsuz bir yazılım oldu. Kesinlikle memnun kaldık ve gelecekteki projelerde tekrar birlikte çalışmayı dört gözle bekliyoruz.',
-    image:'',
+    content:
+      "Appzisoft  ile birlikte çalıştığımız proje, gerçekten harika bir deneyimdi. Jena Karlis ve ekibi, isteklerimizi anlamak ve ihtiyaçlarımıza uygun bir çözüm sunmak için sürekli çaba gösterdiler. Proje süresince sağlam bir iletişim ve proaktif bir yaklaşım sergilediler. Sonuç, kullanıcı dostu ve sorunsuz bir yazılım oldu. Kesinlikle memnun kaldık ve gelecekteki projelerde tekrar birlikte çalışmayı dört gözle bekliyoruz.",
+    image: "",
   },
   {
-    name: 'Ayşe Kaya',
-    position: 'Proje Yöneticisi',
-    company: 'SoftSolutions Ltd.',
+    name: "Ayşe Kaya",
+    position: "Proje Yöneticisi",
+    company: "SoftSolutions Ltd.",
     rating: 4,
-    content: 'Appzisoft  ekibi, proje sürecinde ihtiyaçlarımıza uygun bir yazılım geliştirmek için önemli bir özveri gösterdi. İşbirlikçi yaklaşımları ve esnekliği sayesinde, projenin yönünü belirlemek ve gerektiğinde değişiklik yapmak kolaydı. Projenin sonuçları oldukça tatmin ediciydi ve kullanıcıların geri bildirimleri de olumlu oldu.',
-    image:'',
+    content:
+      "Appzisoft  ekibi, proje sürecinde ihtiyaçlarımıza uygun bir yazılım geliştirmek için önemli bir özveri gösterdi. İşbirlikçi yaklaşımları ve esnekliği sayesinde, projenin yönünü belirlemek ve gerektiğinde değişiklik yapmak kolaydı. Projenin sonuçları oldukça tatmin ediciydi ve kullanıcıların geri bildirimleri de olumlu oldu.",
+    image: "",
   },
-  
-  
 
+  {
+    name: "Fatma Can",
+    position: "QA Engineer",
+    company: "TestPro",
+    rating: 4,
+    content:
+      "Olpers ile yaptığımız proje, kalite kontrol süreçlerimizdeki katkılarıyla önemli bir başarıydı. Ekip, yazılımın testlerini titizlikle yürüttü ve hataları düzeltmek için hızlı bir şekilde geri bildirim sağladı. Proje sonunda, kullanıcı deneyimini önemli ölçüde geliştiren güvenilir bir ürün sunduk.",
+  },
+  {
+    name: "Ahmet Demir",
+    position: "Software Architect",
+    company: "SmartSolutions",
+    rating: 5,
+    content:
+      "Olpers projesi, yazılım mimarisini oluşturmak ve uygulamak açısından büyük bir başarıydı. Ekip, güçlü teknik bilgisi ve projeye olan bağlılığıyla süreci yönetti. Sonuçta, yüksek kaliteli, ölçeklenebilir ve performanslı bir yazılım elde ettik. Olpers ekibine teşekkür ederiz!",
+  },
 ];
