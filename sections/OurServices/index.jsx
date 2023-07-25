a301797c7993dfe9cb3b01d3b18cecbe9dc63f3b
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-07-25 17:41:06 +0300

    updated services

--- sections/OurServices/index.jsx
+++ sections/OurServices/index.jsx
@@ -22,86 +22,85 @@ const Services = (props) => {
   const router = useRouter();
 
   return (
-    <Flex gap={5} direction={"column"} p={{ base: 3, md: 5, lg: 8 }}>
-      <Flex
-        direction={"column"}
-        gap={5}
-        mt={2}
-        mx={5}
-        userSelect={"none"}
-        p={5}
-        //bg={useColorModeValue("#F5F5F5", "#fff")}
-        boxShadow={"rgba(17, 17, 26, 0.1) 0px 0px 16px;"}
-        borderRadius={"10px"}
-        cursor="pointer"
-        role={"group"}
-        _hover={{
-          boxShadow: `rgba(84, 190, 195, 0.4) 0px 2px 4px, rgba(84, 190, 195, 0.3) 0px 7px 13px -3px, rgba(84, 190, 195, 0.2) 0px -3px 0px inset;`,
-        }}
-        rounded="md"
-        marginInline="auto"
-        onClick={() => {
-          router.push("/tr/service/[id]", `/tr/service/${href}`, {
-            shallow: true,
-          });
-        }}
-      >
-        <Flex justifyContent={"flex-start"} data-aos={"zoom-out"}>
-          <Image
-            id={id}
-            alt={title}
-            height={70}
-            width={70}
-            src={img}
-            draggable={false}
-          />
-        </Flex>
-        <Flex justifyContent={"center"}>
-          <Heading
-            as={"h2"}
-            data-aos="zoom-in"
-            textTransform={"uppercase"}
-            fontSize={{ base: "15px", md: "16px" }}
-            isTruncated
-          >
-            {title}
-          </Heading>
-        </Flex>
-
-        <Text
-          data-aos="zoom-in-up"
-          fontSize={{ base: "15px", md: "16px" }}
-          p={3}
-          fontFamily={"'Open Sans', sans-serif, Arial, Helvetica"}
-        >
-          {content}
-        </Text>
-
-        <Flex justifyContent={"center"} mt={4}>
-          <Link
-            href="/tr/service/[id]"
-            as={`/tr/service/${href}`}
-            shallow={true}
-            passHref
-          >
-            <Button
-              variant={"outline"}
-              color="primary.100"
-              rightIcon={<BsArrowRight />}
-              w={"full"}
-              h={"50px"}
-              _groupHover={{
-                boxShadow: `rgba(84, 190, 195, 0.4) 0px 2px 4px, rgba(84, 190, 195, 0.3) 0px 7px 13px -3px, rgba(84, 190, 195, 0.2) 0px -3px 0px inset;`,
-              }}
-            >
-              İncele
-            </Button>
-          </Link>
-        </Flex>
-        <Text mt={2} textAlign="center" fontSize="sm">
-          Size Özel Çözümlerimizle İşinizi Dijital Dünyada Öne Çıkarıyoruz!
-        </Text>
+    <Flex
+      direction={"column"}
+      gap={5}
+      mt={2}
+      mx={5}
+      userSelect={"none"}
+      h={550}
+      p={5}
+      //bg={useColorModeValue("#F5F5F5", "#fff")}
+      boxShadow={"rgba(17, 17, 26, 0.1) 0px 0px 16px;"}
+      borderRadius={"10px"}
+      cursor="pointer"
+      role={"group"}
+      _hover={{
+        boxShadow: `rgba(84, 190, 195, 0.4) 0px 2px 4px, rgba(84, 190, 195, 0.3) 0px 7px 13px -3px, rgba(84, 190, 195, 0.2) 0px -3px 0px inset;`,
+      }}
+      rounded="md"
+      marginInline="auto"
+      onClick={() => {
+        router.push("/tr/service/[id]", `/tr/service/${href}`, {
+          shallow: true,
+        });
+      }}
+    >
+      <Flex justifyContent={"flex-start"} data-aos={"zoom-out"}>
+        <Image
+          id={id}
+          alt={title}
+          height={70}
+          width={70}
+          src={img}
+          draggable={false}
+        />
       </Flex>
+      <Flex justifyContent={"center"}>
+        <Heading
+          as={"h2"}
+          data-aos="zoom-in"
+          textTransform={"uppercase"}
+          fontSize={{ base: "15px", md: "16px" }}
+          isTruncated
+        >
+          {title}
+        </Heading>
+      </Flex>
+
+      <Text
+        data-aos="zoom-in-up"
+        fontSize={{ base: "15px", md: "16px" }}
+        p={3}
+        fontFamily={"'Open Sans', sans-serif, Arial, Helvetica"}
+      >
+        {content}
+      </Text>
+
+      <Flex justifyContent={"center"} mt={4}>
+        <Link
+          href="/tr/service/[id]"
+          as={`/tr/service/${href}`}
+          shallow={true}
+          passHref
+        >
+          <Button
+            variant={"outline"}
+            color="primary.100"
+            rightIcon={<BsArrowRight />}
+            w={"full"}
+            h={"50px"}
+            _groupHover={{
+              boxShadow: `rgba(84, 190, 195, 0.4) 0px 2px 4px, rgba(84, 190, 195, 0.3) 0px 7px 13px -3px, rgba(84, 190, 195, 0.2) 0px -3px 0px inset;`,
+            }}
+          >
+            İncele
+          </Button>
+        </Link>
+      </Flex>
+      <Text mt={2} textAlign="center" fontSize="sm">
+        Size Özel Çözümlerimizle İşinizi Dijital Dünyada Öne Çıkarıyoruz!
+      </Text>
     </Flex>
   );
 };
@@ -145,14 +144,7 @@ function OurServices({ targetId }) {
         </Flex>
       </Flex>
 
-      <Flex
-        as={"article"}
-        direction={"column"}
-        gap={5}
-        w="100vw"
-        h="auto"
-        data-aos="zoom-in-up"
-      >
+      <Flex as={"article"} direction={"column"} gap={5} data-aos="zoom-in-up">
         <SimpleGrid
           columns={{ sm: 1, md: 2, lg: 2, xl: 2, "2xl": 4 }}
           spacing={2}
