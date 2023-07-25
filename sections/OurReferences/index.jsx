1402e2bbfc5f46362cd71b18d88128a562af3bc1
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-07-25 17:10:34 +0300

    updated

--- sections/OurReferences/index.jsx
+++ sections/OurReferences/index.jsx
@@ -12,62 +12,67 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { projects } from "../../constants/projects";
 import Link from "next/link";
+import Slider from "react-slick";
 
 const defaultWidth = 175;
 const defaultHeight = 75;
 
 export default function OurReferences() {
   const [references, setReferences] = useState(projects);
-
+  const settings = {
+    dots: false,
+    infinite: true,
+    lazyLoad: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    easing: 'easeOutElastic',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
   return (
-    <Box>
-      <Flex
-        id="services-top"
-        direction={"row"}
-        justifyContent={"space-between"}
-        px={{ base: 10, md: 20 }}
-      >
-        <Flex direction={"column"} justify={"center"} gap={3}>
-          <Heading
-            as="h2"
-            fontSize={24}
-            pos="relative"
-            _before={{
-              content: `""`,
-              borderRadius: "50px",
-              position: "absolute",
-              backgroundColor: "#000",
-              width: "4px",
-              height: "100%",
-              left: "-10px",
-            }}
-          >
-            Referanslar
-          </Heading>
-        </Flex>
-        <Flex display={{ base: "none", md: "initial" }}>
-          <Link href={"/tr/services"} passHref>
-            <Button variant={"outline"}>Tüm Referanslar</Button>
-          </Link>
-        </Flex>
-      </Flex>
-
-      <Center>
-        <SimpleGrid
-          columns={{
-            base: 2,
-            sm: 3,
-            md: 4,
-            lg: 6,
-          }}
-          spacingX={{
-            base: 16,
-            lg: 24,
-          }}
-          spacingY={20}
-          mt={6}
+    <>
+      <Box>
+        <Flex
+          id="services-top"
+          direction={"row"}
+          justifyContent={"space-between"}
+          px={{ base: 10, md: 20 }}
         >
+          <Flex direction={"column"} justify={"center"} gap={3}>
+            <Heading
+              as="h2"
+              fontSize={24}
+              pos="relative"
+              _before={{
+                content: `""`,
+                borderRadius: "50px",
+                position: "absolute",
+                backgroundColor: "#000",
+                width: "4px",
+                height: "100%",
+                left: "-10px",
+              }}
+            >
+              Referanslar
+            </Heading>
+          </Flex>
+          <Flex display={{ base: "none", md: "initial" }}>
+            <Link href={"/tr/services"} passHref>
+              <Button variant={"outline"}>Tüm Referanslar</Button>
+            </Link>
+          </Flex>
+        </Flex>
+
+        <Slider {...settings}>
           {references.map((ref) => (
             <div key={ref.id}>
               <Image
@@ -78,24 +83,21 @@ export default function OurReferences() {
                 mx={2}
                 opacity={0.7}
                 style={{ objectFit: "contain", filter: "saturate(2)" }}
-                sx={{
-                  "@media (min-width: 768px)": {
-                    width: `${ref.w}px`,
-                    height: `${ref.h}px`,
-                  },
-                }}
               />
             </div>
           ))}
-        </SimpleGrid>
-      </Center>
-      <Center>
-        <Flex display={{ base: "initial", md: "none" }}>
-          <Link href={"/tr/services"} passHref>
-            <Button variant={"outline"}>Tüm Referanslar</Button>
-          </Link>
-        </Flex>
-      </Center>
-    </Box>
+        </Slider>
+
+        <Center>
+          <Flex display={{ base: "initial", md: "none" }}>
+            <Link href={"/tr/services"} passHref>
+              <Button variant={"outline"}>Tüm Referanslar</Button>
+            </Link>
+          </Flex>
+        </Center>
+      </Box>
+
+      <></>
+    </>
   );
 }
