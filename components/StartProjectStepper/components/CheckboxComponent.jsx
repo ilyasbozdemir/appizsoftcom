eb5fdf52832e934cf0818ca275370a4ad77da4f6
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-08-05 12:38:51 +0300

    updated

---
+++ components/StartProjectStepper/components/CheckboxComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RadioComponent() {
+  return (
+    <div>RadioComponent</div>
+  )
+}
+
+export default RadioComponent
---
+++ components/StartProjectStepper/components/FileUpload.jsx
@@ -0,0 +1,87 @@
+import { Flex, FormControl, FormLabel, Input, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Button } from "react-scroll";
+
+function FileUpload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleRemoveClick = (e) => {
+    e.stopPropagation();
+    setSelectedFile(null);
+  };
+  return (
+    <>
+      <FormControl>
+        <FormLabel>Projenize ait dosya varsa ekleyiniz.</FormLabel>
+
+        <Flex
+          direction="column"
+          align="center"
+          justify="center"
+          p="6"
+          border="2px dashed #ccc"
+          borderRadius="8px"
+          cursor="pointer"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+        >
+          <FormLabel htmlFor="fileInput">
+            <Text fontSize="lg">
+              Projenize ait dosya varsa ekleyiniz veya sürükleyip bırakınız.
+            </Text>
+          </FormLabel>
+          <Input
+            id="fileInput"
+            type="file"
+            display={"none"}
+            onChange={handleFileChange}
+          />
+        </Flex>
+
+        {selectedFile ? (
+          <Flex
+            justifyContent={"flex-start"}
+            gap={5}
+            mt={10}
+            direction="column"
+            p="6"
+          >
+            <Flex direction={"column"}>
+              <Text fontSize="lg" fontWeight="bold">
+                Seçili dosya
+              </Text>
+
+              <Text fontSize="lg" fontWeight="bold">
+                • {selectedFile.name}
+              </Text>
+            </Flex>
+            <Box>
+              <Button colorScheme="red" onClick={handleRemoveClick}>
+                Kaldır
+              </Button>
+            </Box>
+          </Flex>
+        ) : (
+          <></>
+        )}
+      </FormControl>
+    </>
+  );
+}
+
+export default FileUpload;
