ee375dbb14d2527d121ca405dac7f2a80f66e127
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-08-08 00:23:37 +0300

    updated

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -17,7 +17,13 @@ export default class MyDocument extends Document {
     return (
       <Html lang={langValue || site.lang} prefix="og: http://ogp.me/ns#">
         <Head>
-          <link rel="canonical" href={site.baseUrl} />
+
+          {
+            /*
+ <link rel="canonical" href={site.baseUrl} />
+             */
+          }
+         
 
           <meta charSet="utf-8" />
 
