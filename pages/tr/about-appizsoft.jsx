c896ca53ecae06316d9fdbac5772b5e8cd4d524e
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-08-08 17:04:55 +0300

    updated

--- pages/tr/about-appizsoft.jsx
+++ pages/tr/about-appizsoft.jsx
@@ -1,25 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { Box, Center, Flex, Heading, Text, VStack } from "@chakra-ui/react";
 import PagesBreadcrumb from "../../components/shared/PagesBreadcrumb";
 import Head from "next/head";
+
+
+
 const AboutUsCTA = () => {
-  const router = useRouter();
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    // İlk renderda ekran boyutuna göre kontrol yapılır
+    handleWindowSize();
+
+    // Ekran boyutu değiştiğinde kontrol yapılır
+    window.addEventListener("resize", handleWindowSize);
+
+    // Temizleme fonksiyonu
+    return () => {
+      window.removeEventListener("resize", handleWindowSize);
+    };
+  }, []);
+
+  const handleWindowSize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
   return (
     <>
-      <Flex h={"250px"}   bgGradient="linear(to-l, #667eea, #54BEC3)" w="100%" color="white" justify={"center"}>
-        <Center>
-          <Flex direction={"column"}>
-            <Text as="h1" fontSize={65}>Hakkımızda</Text>
-            <Center>
-              <PagesBreadcrumb currentPage={"Hakkımızda"} />
-            </Center>
-          </Flex>
-        </Center>
+      <Flex mt={4} direction={"column"}>
+        {!isMobile && (
+          <>
+            <Flex
+              as="section"
+              h={"350px"}
+              bgGradient="linear(to-l, #667eea, #54BEC3)"
+              w="100%"
+              color="white"
+              justify={"center"}
+            >
+              <Center>
+                <Flex direction={"column"}>
+                  <Text fontSize={65}>Hakkımzda</Text>
+                  <Center>
+                    <PagesBreadcrumb currentPage={"Hakkımzda"} />
+                  </Center>
+                </Flex>
+              </Center>
+            </Flex>
+          </>
+        )}
+
+        {isMobile && (
+          <Box mx={3}>
+            <PagesBreadcrumb currentPage={"Kariyer"} />
+          </Box>
+        )}
       </Flex>
     </>
   );
 };
+
+
 const AboutUsContent = () => {
   return (
     <>
