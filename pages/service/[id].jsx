657dd940ce9bc5bb1c8c57d62bc468032f67ce01
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-08-18 20:36:07 +0300

    updated meta tags

--- pages/service/[id].jsx
+++ pages/service/[id].jsx
@@ -208,6 +208,17 @@ function OurServiceDetailPage({ service }) {
     <>
       <Head>
         <title>{title}</title>
+        <link
+          rel="alternate"
+          hreflang="x-default"
+          href={`${site.baseUrl}`}
+        />
+
+        <link rel="alternate" hreflang="tr" href={`${site.baseUrl}/${service.href}`} />
+        <link rel="alternate" hreflang="en" href={`${site.baseUrl}/en/${service.href}`} />
+
+        <link rel="canonical" href={`${site.baseUrl}/${service.href}`} />
+
         <meta itemprop="description" content={desc} />
         <meta name="description" content={desc} />
         <meta name="publisher" content={publisher} />
