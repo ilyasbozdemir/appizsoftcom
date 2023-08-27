7b7d9083feed821e09f2977385d409ccd58769fb
ilyasBozdemir <bozdemir.ib70@gmail.com>
2023-08-27 12:54:20 +0300

    updated

---
+++ generateSitemap.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const urls = [
+  { loc: 'https://www.appizsoft.com/', lang: 'tr' },
+  { loc: 'https://www.appizsoft.com/en/', lang: 'en' },
+  { loc: 'https://www.appizsoft.com/about-appizsoft', lang: 'tr' },
+  { loc: 'https://www.appizsoft.com/en/about-appizsoft', lang: 'en' },
+];
+
+const currentTime = new Date();
+const year = currentTime.getFullYear();
+const month = String(currentTime.getMonth() + 1).padStart(2, "0");
+const day = String(currentTime.getDate()).padStart(2, "0");
+const hours = String(currentTime.getHours()).padStart(2, "0");
+const minutes = String(currentTime.getMinutes()).padStart(2, "0");
+const seconds = String(currentTime.getSeconds()).padStart(2, "0");
+const timezoneOffset = "03:00"; // UTC saat dilimi
+
+const formattedTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}+${timezoneOffset}`;
+
+const generateSitemap = () => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+${urls.map(({ loc, lang }) => `
+  <url>
+    <loc>${loc}</loc>
+    <xhtml:link rel="alternate" hreflang="${lang}" href="${loc}" />
+    <lastmod>${formattedTime}</lastmod>
+  </url>`).join('\n')}
+</urlset>`;
+
+  fs.writeFileSync('public/page-sitemap.xml', sitemap);
+};
+
+
+generateSitemap();
